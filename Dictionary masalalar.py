fcdd86624e30e35eeff631638a7faf5e58107c5e
Luiza <luizaolimjanova@gmail.com>
2025-11-29 17:20:28 +0500

    Add files via upload

---
+++ Dictionary masalalar.py
@@ -0,0 +1,485 @@
+                     #1-MASALA
+# student_grades = {"Matematika": 90, "Fizika": 85, "Informatika": 95}
+# #1-masala
+# student_grades["Kimyo"] = 88
+# result1 = student_grades
+# print("1-vazifa natijasi:", result1)
+# #2-masala
+# student_grades["Fizika"] = 87
+# result2 = student_grades
+# print("2-vazifa natijasi:", result2)
+# #3-masala
+# del student_grades["Informatika"]
+# result3 = student_grades
+# print("3-vazifa natijasi:", result3)
+# #4-masala
+# result4 = sum(student_grades.values())
+# print("4-vazifa natijasi:", result4)
+# #5-masala
+# result5 = sum(student_grades.values()) / len(student_grades)
+# print("5-vazifa natijasi:", result5)
+# #6-masala
+# result6 = max(student_grades.values())
+# print("6-vazifa natijasi:", result6)
+# #7-masala
+# result7 = min(student_grades.values())
+# print("7-vazifa natijasi:", result7)
+# #8-masala
+# result8 = list(student_grades.keys())
+# print("8-vazifa natijasi:", result8)
+# #9-masala
+# result9 = list(student_grades.values())
+# print("9-vazifa natijasi:", result9)
+# #10-masala
+# result10 = len(student_grades) == 0
+# print("10-vazifa natijasi:", result10)
+
+                  #2-MASALA
+# fruits = {"olma": 5000, "banan": 12000, "uzum": 8000}
+#
+# # 1-vazifa:
+# fruits["anor"] = 1000
+# result1 = fruits
+# print("1-vazifa natijasi:", result1)
+# # 2-vazifa:
+# fruits["banan"] = 15000
+# result2 = fruits
+# print("2-vazifa natijasi:", result2)
+# # 3-vazifa:
+# del fruits["uzum"]
+# result3 = fruits
+# print("3-vazifa natijasi:", result3)
+# # 4-vazifa:
+# result4 = sum(fruits.values())
+# print("4-vazifa natijasi:", result4)
+# # 5-vazifa:
+# result5 = max(fruits, key=fruits.get)
+# print("5-vazifa natijasi:", result5)
+# # 6-vazifa:
+# result6 = min(fruits, key=fruits.get)
+# print("6-vazifa natijasi:", result6)
+# # 7-vazifa:
+# result7 = sorted(fruits.values())
+# print("7-vazifa natijasi:", result7)
+# # 8-vazifa:
+# result8 = len(fruits)
+# print("8-vazifa natijasi:", result8)
+# # 9-vazifa:
+# result9 = fruits.get("olma")
+# print("9-vazifa natijasi:", result9)
+# # 10-vazifa:
+# result10 = sorted(fruits.keys())
+# print("10-vazifa natijasi:", result10)
+                     #3-MASALA
+# cities = {"Toshkent": 2500000, "Samarqand": 550000, "Buxoro": 280000}
+# #1-vazifa
+# cities["Andijon"]=600000
+# result1=cities
+# print("1-vazifa natijasi:",result1)
+# #2-vazifa
+# cities["Toshkent"]=2600000
+# result2=cities
+# print("2-vazifa natijasi:",result2)
+# #3-vazifa
+# del cities["Buxoro"]
+# result3=cities
+# print("3-vazifa natijasi:",result3)
+# #4-vazifa
+# result4=sum(cities.values())
+# print("4-vazifa natijasi:",result4)
+# #5-vazifa
+# result5=sum(cities.values())/len(cities)
+# print("5-vazifa natijasi:",result5)
+# #6-vazifa
+# result6=max(cities,key=cities.get)
+# print("6-vazifa natijasi:",result6)
+# #7-vazifa
+# result7=min(cities,key=cities.get)
+# print("7-vazifa natijasi:",result7)
+# #8-vazifa
+# result8=list(cities.keys())
+# print("8-vazifa natijasi:",result8)
+# #9-vazifa
+# result9=sorted(cities.values())
+# print("9-vazifa natijasi:",result9)
+# #10-vazifa
+# result10=len(cities)==0
+# print("10-vazifa natijasi:",result10)
+
+                    #4-MASALA
+# books = {"Alximik": "Paulo Koelyo", "Shaytanat": "Tohir Malik", "1984": "George Orwell"}
+# #1-vazifa
+# #books["O'tkan kunlar","Abdulla Qodiriy"]
+# #result1=books
+# #print("1-vazifa natijasi:",result1)
+# #2-vazifa
+# books["Alximik"]="Paulo Coelho"
+# result2=books
+# print("2-vazifa natijasi:",result2)
+# #3-vazifa
+# del books["1984"]
+# result3=books
+# print("3-vazifa natijasi:",result3)
+# #4-vazifa
+# result4=len(books)
+# print("4-vazifa natijasi:",result4)
+# #5-vazifa
+# result5=list(books.values())
+# print("5-vazifa natijasi:",result5)
+# #6-masala
+# result6=sorted(books.keys())
+# print("6-vazifa natijasi:",result6)
+# #7-vazifa
+# result7=books.get("Shaytanat")
+# print("7-vazifa natijasi:",result7)
+# #8-vazifa
+# result8=len(books)
+# print("8-vazifa natijasi:",result8)
+# #9-vazifa
+# if "O'tkan kunlar" in books:
+#     print("Ha,bor")
+# else:
+#     print("Yo'q")
+# #10-vazifa
+# books.clear()
+# print("10-vazifa natijasi:")
+
+                 #5-MASALA
+# currencies = {"USD": 12600, "EUR": 13500, "RUB": 140}
+# #1-vazifa
+# currencies["GBP"]=16000
+# result1=currencies
+# print("1-vazifa natijasi:",result1)
+# #2-vazifa
+# currencies["USD"]=12700
+# result2=currencies
+# print("2-vazifa natijasi:",result2)
+# #3-vazifa
+# del currencies["RUB"]
+# result3=currencies
+# print("3-vazifa natijasi:",result3)
+# #4-vazifa
+# result4=sum(currencies.values())
+# print("4-vazifa natijasi:",result4)
+# #5-vazifa
+# result5=max(currencies,key=currencies.get)
+# print("5-vazifa natijasi:",result5)
+# #6-vazifa
+# result6=min(currencies,key=currencies.get)
+# print("6-vazifa natijasi:",result6)
+# #7-vazifa
+# result7=list(currencies.keys())
+# print("7-vazifa natijasi:",result7)
+# #8-vazifa
+# result8=sorted(currencies.values())
+# print("8-vazifa natijasi:",result8)
+# #9-vazifa
+# result9=currencies.get()
+# print("9-vazifa natijasi:",result9)
+# #10-vazifa
+# result10=len(currencies)==0
+# print("10-vazifa natijasi:",result10)
+
+                        #6-MASALA
+# inventory = {"suv": 50, "non": 100, "shokolad": 20}
+# #1-vazifa
+# inventory["cola"]=30
+# result1=inventory
+# print("1-vazifa natijasi:",result1)
+# #2-vazifa
+# inventory["non"]=80
+# result2=inventory
+# print("2-vazifa natijasi:",result2)
+# #3-vazifa
+# del inventory["shokolad"]
+# result3=inventory
+# print("3-vazifa natijasi:",result3)
+# #4-vazifa
+# result4=sum(inventory.values())
+# print("4-vazifa natijasi:",result4)
+# #5-vazifa
+# result5=max(inventory,key=inventory.get)
+# print("5-vazifa natijasi:",result5)
+# #6-vazifa
+# result6=min(inventory,key=inventory.get)
+# print("6-vazifa natijasi:",result6)
+# #7-vazifa
+# result7=list(inventory.keys())
+# print("7-vazifa natijasi:",result7)
+# #8-vazifa
+# result8=sorted(inventory.values())
+# print("8-vazifa natijasi:",result8)
+# #9-vazifa
+# result9=inventory.get()
+# print("9-vazifa natijasi:",result9)
+# #10-vazifa
+# result10=len(inventory)==0
+# print("10-vazifa natijasi:",result10)
+
+                        #7-MASALA
+# athletes = {"Ali": 150, "Vali": 120, "Sardor": 180}
+# #1-vazifa
+# athletes["Sardor"]=130
+# result1=athletes
+# print("1-vazifa natijasi:",result1)
+# #2-vazifa
+# athletes["Vali"]=140
+# result2=athletes
+# print("2-vazifa natijasi:",result2)
+# #3-vazifa
+# del athletes["Sardor"]
+# result3=athletes
+# print("3-vazifa natijasi:",result3)
+# #4-vazifa
+# result4=sum(athletes.values())
+# print("4-vazifa natijasi:",result4)
+# #5-vazifa
+# result5=sum(athletes.values())/len(athletes)
+# print("5-vazifa natijasi:",result5)
+# #6-vazifa
+# result6=max(athletes,key=athletes.get)
+# print("6-vazifa natijasi:",result6)
+# #7-vazifa
+# result7=min(athletes,key=athletes.get)
+# print("7-vazifa natijasi:",result7)
+# #8-vazifa
+# result8=list(athletes.keys())
+# print("8-vazifa natijasi:",result8)
+# #9-vazifa
+# result9=sorted(athletes.values())
+# print("9-vazifa natijasi:",result9)
+# #10-vazifa
+# result10=len(athletes)==0
+# print("10-vazifa natijasi:",result10)
+
+                       #8-MASALA
+# menu = {"osh": 25000, "shashlik": 30000, "somsa": 5000}
+# #1-vazifa
+# menu["lag'mon"]=20000
+# result1=menu
+# print("1-vazifa natijasi:",result1)
+# #2-vazifa
+# menu["somsa"]=6000
+# result2=menu
+# print("2-vazifa natijasi:",result2)
+# #3-vazifa
+# del menu["somsa"]
+# result3=menu
+# print("3-vazifa natijasi:",result3)
+# #4-vazifa
+# result4=sum(menu.values())
+# result4=menu
+# print("4-vazifa natijasi:",result4)
+# #5-vazifa
+# result5=max(menu,key=menu.get)
+# print("5-vazifa natijasi:",result5)
+# #6-vazifa
+# result6=min(menu,key=menu.get)
+# print("6-vazifa natijasi:",result6)
+# #7-vazifa
+# result7=list(menu.keys())
+# print("7-vazifa natijasi:",result7)
+# #8-vazifa
+# result8=sorted(menu.values())
+# print("8-vazifa natijasi:",result8)
+# #9-vazifa
+# result9=menu.get()
+# print("9-vazifa natijasi:",result9)
+# #10-vazifa
+# result10=len(menu)==0
+# print("10-vazifa natijasi:",result10)
+
+                   #9-MASALA
+# faculties = {"IT": 120, "Matematika": 80, "Filologiya": 60}
+# #1-vazifa
+# faculties["Fizika"]=90
+# result1=faculties
+# print("1-vazifa natijasi:",result1)
+# #2-vazifa
+# faculties["IT"]=130
+# result2=faculties
+# print("2-vazifa natijasi:",result2)
+# #3-vazifa
+# del faculties["Filologiya"]
+# result3=faculties
+# print("3-vazifa natijasi:",result3)
+# #4-vazifa
+# result4=sum(faculties.values())
+# result5=faculties
+# print("5-vazifa natijasi:",result5)
+# #5-vazifa
+# result5=sum(faculties.values())/len(faculties)
+# print("5-vazifa natijasi:",result5)
+# #6-vazifa
+# result6=max(faculties,key=faculties.get)
+# print("6-vazifa natijasi:",result6)
+# #7-vazifa
+# result7=min(faculties,key=faculties.get)
+# print("7-vazifa natijasi:",result7)
+# #8-vazifa
+# result8=list(faculties.keys())
+# print("8-vazifa natijasi:",result8)
+# #9-vazifa
+# result9=sorted(faculties.values())
+# print("9-vazifa natijasi:",result9)
+# #10-vazifa
+# result10=len(faculties)==0
+# print("10-vazifa natijasi:",result10)
+
+                      #10-MASALA
+# destinations = {"Parij": 500, "Dubay": 300, "Istanbul": 200}
+# #1-vazifa
+# destinations["London"] = 450
+# result1=destinations
+# print("1-vazifa natijasi;",result1)
+# #2-vazifa
+# destinations["Istanbul"]=250
+# result2=destinations
+# print("2-vazifa natijasi;",result2)
+# #3-vazifa
+# del result2["Dubay"]
+# result3=destinations
+# print("3-vazifa natijasi;",result3)
+# #4-vazifa
+# result4=sum(destinations.values())
+# print("4-vazifa natijasi;",result4)
+# #5-vazifa
+# result5=max(destinations,key=destinations.get)
+# print("5-vazifa natijasi;",result5)
+# #6-vazifa
+# result6=min(destinations,key=destinations.get)
+# print("6-vazifa natijasi;",result6)
+# #7-vazifa
+# result7=list(destinations.keys())
+# print("7-vazifa natijasi;",result7)
+# #8-vazifa
+# result8=sorted(destinations.values())
+# print("8-vazifa natijasi;",result8)
+# #9-vazifa
+# result9=destinations.get("Parij")
+# print("9-vazifa natijasi;",result9)
+# #10-vazifa
+# result10=len(destinations)==0
+# print("10-vazifa natijasi;",result10)
+
+                          #11-MASALA
+#phones = {"iPhone 15": 12000000, "Samsung S24": 9000000, "Xiaomi 14": 6000000}
+# #1-vazifa
+# phones["Redmi Note 13"]=4500000
+# result1=phones
+# print("1-vazifa natijasi:",result1)
+# #2-vazifa
+# phones["Samsung S24"]=8500000
+# result2=phones
+# print("2-vazifa natijasi:",result2)
+# #3-vazifa
+# del phones["Xiaomi 14"]
+# result3=phones
+# print("3-vazifa natijasi:",result3)
+# #4-vazifa
+# result4=sum(phones.values())
+# print("4-vazifa natijasi:",result4)
+# #5-vazifa
+# result5=sum(phones.values())/len(phones)
+# print("5-vazifa natijasi:",result5)
+# #6-vazifa
+# result6=max(phones,key=phones.get)
+# print("6-vazifa natijasi:",result6)
+# #7-vazifa
+# result7=min(phones,key=phones.get)
+# print("7-vazifa natijasi:",result7)
+# #8-vazifa
+# result8=sorted(phones.keys())
+# print("8-vazifa natijasi:",result8)
+# #9-vazifa
+# result9=phones.get("iPhone15")
+# print("9-vazifa natijasi:",result9)
+#10-vazifa
+#result10=sorted(phones.values(),reverse=True)
+#print("10-vazifa natijasi:",result10)
+
+
+
+
+# talabalar=["Zuhrajon","Durdona","Luiza","Muhammadjon","Abdulaziz","Sherzod","Akmurod"]
+#
+# for talaba in talabalar:
+#     if talaba == 'Zuhrajon':
+#         print(f"Assalomu alaykum {talaba},o'quvchi")
+#     elif talaba == 'Durdona':
+#         print(f"Assalomu alaykum {talaba},o'quvchi")
+#     elif talaba == 'Luiza':
+#         print(f"Assalomu alaykum {talaba},o'quvchi")
+#     elif talaba == 'Muhammadjon':
+#         print(f"Assalomu alaykum {talaba},o'quvchi")
+#     elif talaba == 'Abdulaziz':
+#         print(f"Assalomu alaykum {talaba},o'quvchi")
+#     elif talaba == 'Sherzod':
+#         print(f"Assalomu alaykum {talaba},o'quvchi")
+#     elif talaba == 'Akmurod':
+#         print(f"Assalomu alaykum {talaba},o'quvchi")
+#
+
+# talabalar = ["Zuhrajon", "Durdona", "Luiza", "Muhammadjon", "Abdulaziz", "Sherzod", "Akmurod"]
+#
+#
+# for index in range(len(talabalar)):
+#     print(f"{index}: {talabalar[index]}")
+
+
+
+
+# talablar=["Akmurod,Muhammadjon,Sherzod,Abdulaziz"]
+#
+# for indeks, element in enumerate(talablar,start=1):
+#     print(f"{indeks}. {element}")
+# for indeks in range(len(talablar)):
+#     print(f"{indeks + 1}: {talablar[indeks]}")
+#
+                #1-MASALA:
+# print("1dan 10gacha sonlar:")
+# for son in range (1,11):
+#     print(son)
+#
+#                #2-MASALA
+# print("Do'stlarim")
+# ismlar = ["Ali", "Vali", "Hasan", "Malika", "Zuhra"]
+# for ism in ismlar:
+#     print(f"Salom,{ism}")
+#
+#                 #3-MASALA
+# print("1 dan 100 gacha bo'lgan juft sonlar")
+# for son in range (1,101):
+#     if son%2==0:
+#         print(son,end=" ")
+
+                  #4-MASALA
+# yigindi=0
+# for son in range(1,51):
+#     yigindi=yigindi+son
+# print(f"Yig'indi: {yigindi}")
+                  #5-MASALA
+# matn=input("Matn kiriting:")
+# count=0
+# for belgi in matn:
+#     count=count+1
+# print(f"{count} ta belgi")
+                         #6-MASALA
+# print("1 dan 50 gacha 3 ga bo'linadigan sonlar")
+# for son in range (1,51):
+#     if son % 3 == 0:
+#         print(son,end=",")
+
+                #7-MASALA
+# matn=input("Matn kiriting:").lower()
+#
+# unli=["a","e","i","o","u"]
+# hisoblagich=0
+# for belgi in matn:
+#     if belgi in unli:
+#         hisoblagich=hisoblagich+1
+# print(hisoblagich)
+
+
+
+
